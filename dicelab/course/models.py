5c1fb23951338cf73545256d744212070224073c
Woo-Yeol <king199276@gmail.com>
2021-09-20 19:56:07 +0900

    Fix Object.save

--- dicelab/course/models.py
+++ dicelab/course/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=10, null=False, default='')
+    name = models.CharField(max_length=100, null=False, default='')
+    semester = models.CharField(max_length=200, null=False, default='')
--- dicelab/course/tasks.py
+++ dicelab/course/tasks.py
@@ -5,6 +5,7 @@ import json
 import urllib3
 from typing import Dict
 from json import loads
+from .forms import CoursesCreationForm
 
 
 http = urllib3.PoolManager()
@@ -26,53 +27,67 @@ headers = {
 def set_cache():
     cache.set('semester', load_notionAPI_course()['body'])
 
+
+# @shared_task
+def set_data():
+    data = load_notionAPI_course()['body']
+    for d in data:
+        value = {'code': [d['code']], 'name': [
+            d['name']], 'semester': d['semester']}
+        form = CoursesCreationForm(value)
+        print(value)
+        if form.is_valid():
+            form.save()
+            print('success')
+        else:
+            print(form.errors)
+
 # Create your views here.
 
 
 def load_notionAPI_course():
     url = f"https://api.notion.com/v1/databases/{Course_Database_ID}/query"
-    semester = {'2021_Spring': [], '2020_Fall': [], '2020_Spring': [],
-                '2019_Fall': [], '2019_Spring': [], '2018_Fall': [], '2018_Spring': []}
     sorts = [  # 정렬
-        {
-            "property": "period",
-            "direction": "descending"
-        },
         {
             "property": "code",
             "direction": "ascending"
         }
     ]
-    for key in semester:
-        filter = {  # 가져올 데이터 필터
-            "or": [
-                {
-                    "property": "period",
-                    "multi_select": {
-                        "contains": key
-                    }
-                }
-            ]
-        }
-        body = {
-            "filter": filter,
-            "sorts": sorts
-        }
-        response = http.request('POST',
-                                url,
-                                body=json.dumps(body),  # json파일로 인코딩
-                                headers=headers,
-                                retries=False)
-        source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
-        with open("data.json", "w") as f:
-            json.dump(source['results'], f)
-        data = []
-        for r in source['results']:
-            code = r['properties']['code']['title'][0]['plain_text']
-            name = r['properties']['name']['rich_text'][0]['plain_text']
-            semester[key].append(code + ', ' + name)
-        semester[key].sort()
+    filter = {  # 가져올 데이터 필터
+        "or": [
+             {
+                 "property": "name",
+                 "text": {
+                     "is_not_empty": True
+                 }
+             }
+        ]
+    }
+    body = {
+        "filter": filter,
+        "sorts": sorts
+    }
+    response = http.request('POST',
+                            url,
+                            body=json.dumps(body),  # json파일로 인코딩
+                            headers=headers,
+                            retries=False)
+
+    source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
+    data = []
+    for r in source['results']:
+        code = r['properties']['code']['title'][0]['plain_text']
+        name = r['properties']['name']['rich_text'][0]['plain_text']
+        semester = ([l['name']
+                    for l in r['properties']['period']['multi_select']]) if 'period' in r['properties'] else 'None'
+        data.append(
+            {
+                'code': code,
+                'name': name,
+                'semester': semester
+            }
+        )
     return {
         'statusCode': 200,
-        'body': semester
+        'body': data
     }

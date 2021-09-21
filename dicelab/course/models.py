c89435713038bbfd717f7c038cbd1cd76c4fc3f5
Kuhy <kwh1019_@naver.com>
2021-09-21 17:10:09 +0900

    implement news

--- dicelab/course/models.py
+++ dicelab/course/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    code = models.CharField(max_length=10, null=False, default='')
+    name = models.CharField(max_length=100, null=False, default='')
+    semester = models.CharField(max_length=20, null=False, default='')
+
+
+# class Child(models.Model):
+#     name = models.CharField(max_length=200, null=False, default='')
+#     classname = models.CharField(max_length=200, null=False, default='')
+#     grade = models.IntegerField(null=False, default='')
+
+#     def __str__(self):
+#         return self.name
+
+#     class Meta:
+#         ordering = ['classname']
+
+
--- dicelab/news/tasks.py
+++ dicelab/news/tasks.py
@@ -24,12 +24,12 @@ headers = {
 
 @shared_task
 def set_cache():
-    cache.set('semester', load_notionAPI_course()['body'])
+    cache.set('news', load_notionAPI_news()['body'])
 
 # Create your views here.
 
 
-def load_notionAPI_course():
+def load_notionAPI_news():
     url = f"https://api.notion.com/v1/databases/{News_Database_ID}/query"
     filter = {  # 가져올 데이터 필터
         "or": [
@@ -53,32 +53,57 @@ def load_notionAPI_course():
     }
     response = http.request('POST',
                             url,
-                            body=json.dumps(body),  # json파일로 인코딩
+                            body=json.dumps(body),  
                             headers=headers,
                             retries=False)
-
-    source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
-    with open("data.json", "w") as f:
-        json.dump(source['results'], f)
-
+    source: Dict = loads(response.data.decode('utf-8'))
     data = []
-    for r in source['results']:
+    for r in source['results']:  
         page_id = r['url'].split("-")[-1]
+        card = {}
         temp = r['properties']['title']['title']
         title = ''
         for t in temp:
-            title = title + t["plain_text"]
+            title = title + t["plain_text"] # news title
+        card['title'] = title
         page_url = f"https://api.notion.com/v1/blocks/{page_id}/children?page_size=100"
         response = http.request('GET',
-                                page_url,  # json파일로 인코딩
+                                page_url,  
                                 headers=headers,
                                 retries=False)
-        source2: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
-        page = []
+        source2: Dict = loads(response.data.decode('utf-8'))
+        
+        # page inner
+        content = []
         for r2 in source2['results']:
-            if 'bulleted_list_item' in r2:
+            block = {}
+            if 'text' in r2: # text 요소로 시작하는 경우는 노션페이지 데이터가 한 줄이다.
+                text = r2['paragraph']['text'][0]['plain_text']
+                block['parent'] = text
+                content.append(block)
+                break
+            elif 'bulleted_list_item' in r2:
                 text = r2['bulleted_list_item']['text'][0]['plain_text']
+                block['parent'] = text
+                if r2['has_children']:
+                    child_id = r2['id']
+                    child_url = f'https://api.notion.com/v1/blocks/{child_id}/children'
+                    response2 = http.request('GET',
+                                                child_url,  # json파일로 인코딩
+                                                headers=headers,
+                                                retries=False)
+                    source3: Dict = loads(
+                        response2.data.decode('utf-8'))
+                    block_child = [] 
+                    # page_child
+                    for r3 in source3['results']:
+                        subtext = r3['bulleted_list_item']['text'][0]['plain_text']
+                        block_child.append(subtext)
+                    block['child'] = block_child
+                content.append(block)
+        card['content'] = content        
+        data.append(card)        
     return {
         'statusCode': 200,
-        'body': semester
+        'body': data
     }

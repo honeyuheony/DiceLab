ecad7abd3b4b469e3f43f72e66cfbfba6036bd86
Kuhy <kwh1019_@naver.com>
2021-09-22 23:40:05 +0900

    Implement member page, table

--- dicelab/member/admin.py
+++ dicelab/member/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Research_interests, Linked, Graduated, Alumni, Team
 # Register your models here.
+
+admin.site.register(Research_interests)
+admin.site.register(Linked)
+admin.site.register(Graduated)
+admin.site.register(Alumni)
+admin.site.register(Team)
--- dicelab/member/tasks.py
+++ dicelab/member/tasks.py
@@ -4,7 +4,7 @@ import urllib3
 from typing import Dict
 import json
 from json import loads
-from django.core.cache import cache
+from .models import Research_interests, Linked, Graduated, Alumni, Team
 
 http = urllib3.PoolManager()
 Member_Graduate_Database_ID = getattr(
@@ -27,11 +27,40 @@ headers = {
 
 
 @shared_task
-def set_cache():
-    cache.set('graduate', load_notionAPI_member_graduate()['body'])
-    cache.set('ungraduate', load_notionAPI_member_ungraduate()['body'])
-    cache.set('urp', load_notionAPI_member_urp()['body'])
-    cache.set('alumni', load_notionAPI_member_alumni()['body'])
+def set_data():
+    data = load_notionAPI_member_graduate()['body']
+    for d in data:
+        g, created = Graduated.objects.update_or_create(
+            name=d['name'], course=d['course'])
+        if d['admission_date'] != None :
+            g.admission_date = d['admission_date']
+        if d['email'] != None :
+            g.email = d['email']
+        if d['pic'] != None :
+            g.pic = d['pic']
+        for r in d['research_interests']:
+            obj, created = Research_interests.objects.get_or_create(title=r)
+            g.research_interests.add(obj)
+            g.save()
+        if d['linked'] != None :
+            for key, value in d['linked'].items():
+                obj, created = Linked.objects.get_or_create(title=key.replace(" : ", ""), link=value)
+                g.linked.add(obj)
+                g.save()
+
+    data = load_notionAPI_member_alumni()['body']
+    for d in data:
+        a, created = Alumni.objects.update_or_create(
+            name=d['name'], graduate_year=d['graduate_year'])
+        if d['course'] != None :
+            a.course = d['course']
+        else :
+            a.course = ''
+
+        obj, created = Team.objects.get_or_create(title=d['team'])
+        a.team.add(obj)
+        a.save()
+    
 
 
 def load_notionAPI_member_graduate():
@@ -69,13 +98,41 @@ def load_notionAPI_member_graduate():
     source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
     data = []
     for r in source['results']:
-        name = r['properties']['name']['title'][0]['plain_text']
+        try :
+            name = r['properties']['name']['title'][0]['plain_text']
+        except:
+            continue
         course = r['properties']['course']['select']['name']
-        admission_date = r['properties']['admission_date']['select']['name']
+        try:
+            admission_date = r['properties']['admission_date']['select']['name']
+        except:
+            admission_date = None
         research_interests = [l['name']
                               for l in r['properties']['research_interests']['multi_select']]
-        email = r['properties']['email']['rich_text'][0]['plain_text']
-        linked = '-'
+        try :
+            email = r['properties']['email']['email'].replace("@", "'at'")
+        except:
+            email = None
+        links = ''
+        try :
+            temp = r['properties']['linked']['rich_text']
+            for t in temp :
+                links += t['plain_text']
+            links = links.split("\n")
+            link_title = []
+            link_content = []
+            for link in links :
+                if " : " in link :
+                    link_title.append(link)
+                else :
+                    link_content.append(link)
+            linked = dict(zip(link_title, link_content))
+        except:
+            linked = None
+        try:
+            pic = r['properties']['pic']['files'][0]['name']
+        except:
+            pic = None
 
         data.append({
             'name': name,
@@ -83,74 +140,30 @@ def load_notionAPI_member_graduate():
             'admission_date': admission_date,
             'research_interests': research_interests,
             'email': email,
-            'linked': linked
+            'linked': linked,
+            'pic' : pic
         })
     return {
         'statusCode': 200,
         'body': data
     }
 
-
-def load_notionAPI_member_ungraduate():
-    url = f"https://api.notion.com/v1/databases/{Member_Ungraduate_Database_ID}/query"
-    filter = {  # 가져올 데이터 필터
-        "or": [
-            {
-                "property": "course",
-                "select": {
-                    "is_not_empty": True
-                }
-            }
-        ]
-    }
-    sorts = [  # 정렬
-        {
-            "property": "course",
-            "direction": "descending"
-        },
-        {
-            "property": "name",
-            "direction": "ascending"
-        }
-    ]
-    body = {
-        "filter": filter,
-        "sorts": sorts
-    }
-    response = http.request('POST',
-                            url,
-                            body=json.dumps(body),  # json파일로 인코딩
-                            headers=headers,
-                            retries=False)
-
-    source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
-    data = []
-    for r in source['results']:
-        name = r['properties']['name']['title'][0]['plain_text']
-        course = r['properties']['course']['select']['name']
-        research_interests = [l['name']
-                              for l in r['properties']['research_interests']['multi_select']]
-        email = r['properties']['email']['rich_text'][0]['plain_text'] if len(
-            r['properties']['email']['rich_text']) != 0 else ''
-
-        data.append({
-            'name': name,
-            'course': course,
-            'research_interests': research_interests,
-            'email': email
-        })
-    return {
-        'statusCode': 200,
-        'body': data
-    }
-
-
-def load_notionAPI_member_urp():
+def load_notionAPI_member_alumni():
     url = f"https://api.notion.com/v1/databases/{Member_Urp_Database_ID}/query"
+    filter = {  # 가져올 데이터 필터
+            "or": [
+                {
+                    "property": "team",
+                    "select": {
+                        "is_not_empty": True
+                    }
+                }
+            ]
+        }
     sorts = [  # 정렬
         {
             "property": "course",
-            "direction": "descending"
+            "direction": "ascending"
         },
         {
             "property": "team",
@@ -160,76 +173,35 @@ def load_notionAPI_member_urp():
             "property": "name",
             "direction": "ascending"
         }
-    ]
-    team_list = {'3_1': [], '4_1': [], '4_2': []}
-    for key in team_list:
-        filter = {  # 가져올 데이터 필터
-            "or": [
-                {
-                    "property": "team",
-                    "select": {
-                        "equals": key
-                    }
-                }
-            ]
-        }
-        body = {
-            "filter": filter,
-            "sorts": sorts
-        }
-        response = http.request('POST',
-                                url,
-                                body=json.dumps(body),  # json파일로 인코딩
-                                headers=headers,
-                                retries=False)
-        source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
-        for r in source['results']:
-            name = r['properties']['name']['title'][0]['plain_text']
-            team_list[key].append(name)
-        team_list[key] = ', '.join(team_list[key])
-    data = team_list
-    return {
-        'statusCode': 200,
-        'body': data
+    ] 
+    body = {
+        "filter": filter,
+        "sorts": sorts
     }
-
-
-def load_notionAPI_member_alumni():
-    url = f"https://api.notion.com/v1/databases/{Member_Alumni_Database_ID}/query"
-    team_list = {'2020_1': [], '2020_2': [], '2020_3': [], '2021_1': [],
-                 '2021_2': [], '2022_1': [], '2022_2': [], '2022_3': []}
-    sorts = [  # 정렬
-        {
-            "property": "name",
-            "direction": "ascending"
-        }
-    ]
-    for key in team_list:
-        filter = {  # 가져올 데이터 필터
-            "or": [
-                {
-                    "property": "team",
-                    "select": {
-                        "equals": key
-                    }
-                }
-            ]
-        }
-        body = {
-            "filter": filter,
-            "sorts": sorts
-        }
-        response = http.request('POST',
-                                url,
-                                body=json.dumps(body),  # json파일로 인코딩
-                                headers=headers,
-                                retries=False)
-        source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
-        for r in source['results']:
+    response = http.request('POST',
+                            url,
+                            body=json.dumps(body),  # json파일로 인코딩
+                            headers=headers,
+                            retries=False)
+    source: Dict = loads(response.data.decode('utf-8'))  # 자료형 명시
+    data = []
+    for r in source['results']:
+        try :
             name = r['properties']['name']['title'][0]['plain_text']
-            team_list[key].append(name)
-        team_list[key] = ', '.join(team_list[key])
-    data = team_list
+        except:
+            continue
+        try :
+            course = r['properties']['course']['select']['name']
+        except:
+            course = None
+        team = r['properties']['team']['select']['name']
+        graduate_year = r['properties']['graduate_year']['select']['name']
+        data.append({
+            'name': name,
+            'course': course,
+            'team': team,
+            'graduate_year': graduate_year
+        })
     return {
         'statusCode': 200,
         'body': data

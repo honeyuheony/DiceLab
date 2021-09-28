110539de7cac1eee4ae1c2b273c732f9b4f059fc
Woo-Yeol <king199276@gmail.com>
2021-09-29 01:18:06 +0900

    Fix Error : DB Table

--- dicelab/news/models.py
+++ dicelab/news/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
 class News(models.Model):
     title = models.CharField(max_length=100, null=False,
-                            default='', primary_key=True)
+                             default='', primary_key=True)
     date = models.CharField(max_length=10, null=False)
     htmldata = models.CharField(max_length=1000, null=False)
 

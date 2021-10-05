7c801884daf09a10650ddfa4bfd670d7af53290d
Woo-Yeol <king199276@gmail.com>
2021-10-06 03:13:51 +0900

    Modifying Professor Page

--- dicelab/professor/models.py
+++ dicelab/professor/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.db.models.fields.related import ManyToManyField
 
 # Create your models here.
+
+
+class Professor(models.Model):
+    name = CharField(max_length=30)
+
+
+class Introduction(models.Model):
+    position = CharField(max_length=30, null=True)
+    affiliation = CharField(max_length=30, null=True)
+
+
+class Contact(models.Model):
+    location = CharField(max_length=30, null=True)
+    email = CharField(max_length=30, null=True)
+
+
+class Interest(models.Model):
+    field = CharField(max_length=30, null=True)
+
+
+class Research_interests(models.Model):
+    interest = ManyToManyField(Interest)

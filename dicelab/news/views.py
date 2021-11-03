1eb256939a8afb41d8110cd944eee3ab8d6287a5
Kuhy <kwh1019_@naver.com>
2021-11-03 16:44:24 +0900

    Renew css at news

--- dicelab/news/views.py
+++ dicelab/news/views.py
@@ -5,7 +5,7 @@ import json
 
 
 def news(request):
-    set_data()
+    # set_data()
     news = News.objects.all().order_by('-date')
     news_list = []
     for n in news:
@@ -14,5 +14,6 @@ def news(request):
         temp['date'] = n.date
         temp['content'] = json.decoder.JSONDecoder().decode(n.content)
         temp['pic'] = n.pic
+        temp['column_type'] = n.column_type
         news_list.append(temp)
     return render(request, 'news.html', {'news': news_list})

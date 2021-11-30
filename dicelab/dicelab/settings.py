bba412b4c6646a86aec5b24559659c5c6dd58613
Woo-Yeol <king199276@gmail.com>
2021-12-01 01:01:29 +0900

    Save

--- dicelab/dicelab/settings.py
+++ dicelab/dicelab/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

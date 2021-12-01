b90bbe731a0b28c8a8b93c1e41c1349cc6d490b2
Woo-Yeol <king199276@gmail.com>
2021-12-01 16:40:41 +0900

    Modify project order_by('-date')

--- dicelab/project/views.py
+++ dicelab/project/views.py
@@ -5,7 +5,7 @@ from .models import AI_challenge, Project
 
 def project(request):
     set_data()
-    projects = Project.objects.all()
-    ai_challenges = AI_challenge.objects.all()
+    projects = Project.objects.order_by('-date')
+    ai_challenges = AI_challenge.objects.order_by('-date')
 
     return render(request, 'project.html', {'projects': projects, 'ai_challenges': ai_challenges})

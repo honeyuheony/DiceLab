b4c752c2c80393c4f418d99b8ece5b3393f9cda0
Woo-Yeol <king199276@gmail.com>
2021-12-24 16:57:30 +0900

    Modify settings.py for Deploy

--- dicelab/dicelab/settings.py
+++ dicelab/dicelab/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['52.78.175.73']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.78.175.73']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition

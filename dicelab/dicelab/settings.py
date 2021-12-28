eda1ec35246c2b61535879da624096485a0bc938
Woo-Yeol <king199276@gmail.com>
2021-12-28 21:14:39 +0900

    Edit Member Page

--- dicelab/dicelab/settings.py
+++ dicelab/dicelab/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['52.78.175.73']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['52.78.175.73']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

054aea048794803a4af6f9e8a765aa1f1325570d
Woo-Yeol <king199276@gmail.com>
2022-01-04 14:02:56 +0900

    Main Page 13px,bold

--- dicelab/dicelab/settings.py
+++ dicelab/dicelab/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['13.125.28.69', 'dicelab.kr', 'www.dicelab.kr']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['13.125.28.69', 'dicelab.kr', 'www.dicelab.kr']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
